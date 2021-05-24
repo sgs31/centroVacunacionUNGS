36d2deb46dee24e02cbcea3062db028de114920f
sgs31 <57039721+sgs31@users.noreply.github.com>
2021-05-24 19:24:53 -0300

    se agregaron funcionalidades en centroVacunacion y en otras clases mas

--- src/AlmacenVacunas.java
+++ src/AlmacenVacunas.java
@@ -1,4 +1,27 @@
+import java.util.Map;
 
 public class AlmacenVacunas {
+	
+	Map<String, Integer> vacunasVencidas;
+	
+	public void almacenarVacunas(String nombreVacuna, int cantidad, Fecha fechaIngreso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getVacunasDisponibles() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public int getVacunasDisponibles(String vacuna) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Map<String, Integer> getVacunasVencidas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
